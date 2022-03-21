6287f2ddbdb73292910d2224fc7aa515a91b0fcd
GeorgeColeridge <gcb4@st-andrews.ac.uk>
2022-03-21 16:23:37 +0000

    Initial commit

---
+++ Initialising git.R
@@ -0,0 +1,18 @@
+### MT5751 - Project 3 ###
+
+
+# Getting gitty
+
+
+# install packages to work with github
+
+#install.packages("usethis")
+library(usethis)
+
+#install.packages("gitcreds")
+library(gitcreds)
+
+# create_github_token()
+# token = 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
+
+gitcreds_set()
